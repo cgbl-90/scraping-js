f00bb2cfab578b5daf2652a364ae9fefd1479d3f
Carmen Lopez <carmenbaez2000@gmail.com>
2022-12-29 22:21:37 +0100

    Summary

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# scraping-js
- Scraping analytic tool
+# SCRAPING WITH JAVSCRIPT
+
+Express.js (npm i express) is a backend framework for Node.js
+Cheerio picks out elements from a webpage
+Axios = get, post, fetch, etc..
+
+Video tutorials
+
+https://www.youtube.com/watch?v=SpsRJXnRyGE
+
+https://www.youtube.com/watch?v=rlv7ueX4Yjc
+
+https://www.youtube.com/watch?v=_rSZcUU61co
+
+https://www.youtube.com/watch?v=pyMFmKnt6TU
+
+https://www.youtube.com/watch?v=7FjhF6Hy9gY
+
+https://www.youtube.com/watch?v=vsNDg63njpo
+
+https://www.youtube.com/watch?v=OX2LQRP82lk
+
+https://www.youtube.com/watch?v=FaG3txpUJ-U
+
+https://www.youtube.com/watch?v=yB_oh7WgRU8
+
+https://www.youtube.com/watch?v=SZeppJxlQh8
--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ const dd = document.querySelector("pre");
 const url = document.getElementById("url");
 const btn = document.querySelector("button");
 
-function extractText(strToParse, strStart, strFinish) {
-  return strToParse.match(strStart + "(.*?)" + strFinish);
-}
+const headers = {
+  "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 YaBrowser/22.11.5.715 Yowser/2.5 Safari/537.36",
+};
 
 function scrapeData(e) {
   e.preventDefault();
   let xhr = new XMLHttpRequest();
   let response = String;
-  xhr.open("get", url.value, { mode: "no-cors" }, true);
+  xhr.open("get", url.value, { headers: headers }, true);
   xhr.responseType = "text";
   xhr.onload = async () => {
     if (xhr.status == 200) response = xhr.responseText;
